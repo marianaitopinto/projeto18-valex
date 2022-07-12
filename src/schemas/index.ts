dafe0845a0d5652d33ef552a1a335ca47218a7fb
Mariana Ito <marianaitopinto@gmail.com>
2022-07-12 14:01:15 -0300

    feat: creating schema to recharge service

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,9 +1,11 @@
 import createCardSchema from "./createCardSchema";
 import activateCardSchema from "./activateCardSchema";
 import passwordSchema from "./passwordSchema";
+import rechargeSchema from "./rechargeSchema";
 
 export {
     createCardSchema,
     activateCardSchema,
-    passwordSchema
+    passwordSchema,
+    rechargeSchema
 }
