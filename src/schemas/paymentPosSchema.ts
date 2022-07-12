48f8b43685cf664643e6623c902854111e5791ce
Mariana Ito <marianaitopinto@gmail.com>
2022-07-12 14:31:56 -0300

    fix: validating if amount is not 0 to recharge and pay

--- src/schemas/paymentPosSchema.ts
+++ src/schemas/paymentPosSchema.ts
@@ -4,7 +4,7 @@ const paymentPosSchema = joi.object({
     cardId: joi.number().required(),
     businessId: joi.number().required(),
     password: joi.string().regex(/^\d{4}$/).required(),
-    amount: joi.number().required(),
+    amount: joi.number().greater(0).required(),
 });
 
 export default paymentPosSchema;
