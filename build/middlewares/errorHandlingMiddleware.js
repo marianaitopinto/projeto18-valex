9f7ba8a0dd37a5a818fa72a73c76c6c161461735
Mariana Ito <marianaitopinto@gmail.com>
2022-07-13 10:17:53 -0300

    deploy

--- build/middlewares/errorHandlingMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function handleError(error, req, res, next) {
-    if (error.type == "Not Found") {
-        return res.status(404).send(`${error.type}`);
-    }
-    return res.status(500).send("Internal Server Error");
-}
-exports.default = handleError;
